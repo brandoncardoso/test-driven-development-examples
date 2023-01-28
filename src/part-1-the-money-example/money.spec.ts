49728e595e53c5fd9fad5601689993d47784c86f
Brandon Cardoso <cardosobrandon@gmail.com>
2023-01-28 16:47:03 +0900

    rewrite multiplication test to define our expected behaviour

--- src/part-1-the-money-example/money.spec.ts
+++ src/part-1-the-money-example/money.spec.ts
@@ -4,9 +4,9 @@ import { Dollar } from './money'
 describe('Multi-Currency Money', () => {
 	it('should multiply dollars', () => {
 		const five = new Dollar(5)
-		five.times(2)
-		expect(five.amount).to.equal(10)
-		five.times(3)
-		expect(five.amount).to.equal(15)
+		let product: Dollar = five.times(2)
+		expect(product).to.equal(10)
+		product = five.times(3)
+		expect(product).to.equal(15)
 	})
 })
