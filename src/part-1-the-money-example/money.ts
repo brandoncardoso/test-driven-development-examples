76398107bed5ffe874ae6912f21b39680215d954
Brandon Cardoso <cardosobrandon@gmail.com>
2023-01-28 18:25:21 +0900

    refactor currency into the base Money class

--- src/part-1-the-money-example/money.ts
+++ src/part-1-the-money-example/money.ts
@@ -1,21 +1,24 @@
 export abstract class Money {
 	protected amount: number
+	protected currency: string
 
 	constructor(amount: number) {
 		this.amount = amount
 	}
 
 	static dollar(amount: number): Money {
-		return new Dollar(amount)
+		return new Dollar(amount, 'USD')
 	}
 
 	static franc(amount: number): Money {
-		return new Franc(amount)
+		return new Franc(amount, 'CHF')
 	}
 
 	abstract times(multiplier: number): Money
 
-	abstract getCurrency(): string
+	getCurrency(): string {
+		return this.currency
+	}
 
 	equals(money: Money): boolean {
 		return this.amount === money.amount &&
@@ -24,35 +27,23 @@ export abstract class Money {
 }
 
 export class Dollar extends Money {
-	private currency: string
-
-	constructor(amount: number) {
+	constructor(amount: number, currency: string) {
 		super(amount)
-		this.currency = 'USD'
-	}
-
-	getCurrency(): string {
-		return this.currency
+		this.currency = currency
 	}
 
 	times(multiplier: number): Money {
-		return new Dollar(this.amount * multiplier)
+		return Money.dollar(this.amount * multiplier)
 	}
 }
 
 export class Franc extends Money {
-	private currency: string
-
-	constructor(amount: number) {
+	constructor(amount: number, currency: string) {
 		super(amount)
-		this.currency = 'CHF'
-	}
-
-	getCurrency(): string {
-		return this.currency
+		this.currency = currency
 	}
 
 	times(multiplier: number): Money {
-		return new Franc(this.amount * multiplier)
+		return Money.franc(this.amount * multiplier)
 	}
 }
