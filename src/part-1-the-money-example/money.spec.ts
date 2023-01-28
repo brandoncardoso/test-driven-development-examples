7470f56e979a554c8baddb41501a72bd35525234
Brandon Cardoso <cardosobrandon@gmail.com>
2023-01-28 17:21:45 +0900

    add failing franc multiplication test

--- src/part-1-the-money-example/money.spec.ts
+++ src/part-1-the-money-example/money.spec.ts
@@ -12,4 +12,10 @@ describe('Multi-Currency Money', () => {
 		expect(new Dollar(5).equals(new Dollar(5))).to.be.true
 		expect(new Dollar(5).equals(new Dollar(6))).to.be.false
 	})
+
+	it('should multiply francs', () => {
+		const five = new Franc(5)
+		expect(new Franc(10).equals(five.times(2))).to.be.true
+		expect(new Franc(15).equals(five.times(3))).to.be.true
+	})
 })
