4c33627ef6cdff9699b7592fe29d6d84a00ef541
Brandon Cardoso <cardosobrandon@gmail.com>
2023-01-29 02:04:06 +0900

    use our TestCase class to write a test with assertions

--- src/part-2-the-xunit-example/xunit.test.ts
+++ src/part-2-the-xunit-example/xunit.test.ts
@@ -1,6 +1,17 @@
-import { WasRun } from './xunit'
+import { assert } from 'chai'
+import { TestCase, WasRun } from './xunit'
 
 const test = new WasRun('testMethod')
 console.log(test.wasRun)
 test.run()
 console.log(test.wasRun)
+
+class TestCaseTest extends TestCase {
+	testRunning() {
+		const test = new WasRun('testMethod')
+		assert(!test.wasRun)
+		test.run()
+		assert(test.wasRun)
+	}
+}
+new TestCaseTest('testRunning').run()
