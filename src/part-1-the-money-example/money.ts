914a0b0ba6e4825a501307d5acf6d0256a9e78a7
Brandon Cardoso <cardosobrandon@gmail.com>
2023-01-28 20:35:06 +0900

    fix Sum.reduce not actually reducing, pass test

--- src/part-1-the-money-example/money.ts
+++ src/part-1-the-money-example/money.ts
@@ -73,7 +73,7 @@ export class Sum implements Expression {
 	}
 
 	reduce(bank: Bank, to: string): Money {
-		const amount = this.augend.amount + this.addend.amount
+		const amount = this.augend.reduce(bank, to).amount + this.addend.reduce(bank, to).amount
 		return new Money(amount, to)
 	}
 }
