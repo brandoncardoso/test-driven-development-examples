b75e79a0fb6aebbdcb60dbe08a88418bc1b8a46d
Brandon Cardoso <cardosobrandon@gmail.com>
2023-01-29 01:54:09 +0900

    refactor xunit.run to dynamically invoke testMethod

--- src/part-2-the-xunit-example/xunit.ts
+++ src/part-2-the-xunit-example/xunit.ts
@@ -1,15 +1,18 @@
 export class WasRun {
+	name: string
 	wasRun: boolean
 
-	constructor(name: string) {
+	constructor(name: keyof WasRun) {
+		this.name = name
 		this.wasRun = false
 	}
 
-	testMethod(): void {
-		this.wasRun = true
+	testMethod(self: typeof this): void {
+		self.wasRun = true
 	}
 
 	run(): void {
-		this.testMethod()
+		const method = this[this.name as keyof WasRun] as (self: typeof this) => any
+		method(this)
 	}
 }
