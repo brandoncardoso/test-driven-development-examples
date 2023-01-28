642ff6a912007dde3d0757ec6928aec08c7288c5
Brandon Cardoso <cardosobrandon@gmail.com>
2023-01-29 01:58:49 +0900

    refactor to new TestCase parent class

--- src/part-2-the-xunit-example/xunit.ts
+++ src/part-2-the-xunit-example/xunit.ts
@@ -1,18 +1,25 @@
-export class WasRun {
-	name: string
+export class TestCase {
+	protected name: string
+
+	constructor(name: string) {
+		this.name = name
+	}
+
+	run(): void {
+		const method = this[this.name as keyof this] as (self: typeof this) => any
+		method(this)
+	}
+}
+
+export class WasRun extends TestCase {
 	wasRun: boolean
 
-	constructor(name: keyof WasRun) {
-		this.name = name
+	constructor(name: string) {
+		super(name)
 		this.wasRun = false
 	}
 
 	testMethod(self: typeof this): void {
 		self.wasRun = true
-	}
-
-	run(): void {
-		const method = this[this.name as keyof WasRun] as (self: typeof this) => any
-		method(this)
 	}
 }
